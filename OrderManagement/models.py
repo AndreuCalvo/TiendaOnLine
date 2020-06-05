e2abc042fe00e60f271a7f984a6275d684e6f4d8
Andreu Calvo Roy <acalvoroy@uoc.edu>
2020-06-05 10:48:18 +0200

    Add models and change db from sglite3 to mysql

--- OrderManagement/models.py
+++ OrderManagement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name=models.CharField(max_length=30)
+    direction=models.CharField(max_length=50)
+    telephone=models.CharField(max_length=7)
+    email=models.EmailField()
+
+class Article(models.Model):
+    name=models.CharField(max_length=30)
+    section=models.CharField(max_length=40)
+    price=models.IntegerField()
+
+class Order(models.Model):
+    number=models.IntegerField()
+    date=models.DateField()
+    delivered=models.BooleanField()
